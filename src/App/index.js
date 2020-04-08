1e45f870debad27ef8e6805afd1d4546920945c7
Jaikangam Malangmei <jaikangam12@gmail.com>
2020-04-09 00:26:49 +0530

    Added Style component NEW Package

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import WelcomeMessage from './WelcomeMessage'
+import styled, {css} from 'styled-components';
 import './App.css';
+
+const MyButton = styled.div`
+  color:green;
+
+  ${props => props.primary && css`
+    color: palevioletred;
+  `}
+`
+
+// A new component based on Button, but with some override styles
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <div> Hello World</div>
+    <div> 
+      <WelcomeMessage />
+      <MyButton> Hello </MyButton>
+      <MyButton primary> Hello </MyButton>
+      <TomatoButton>Tomato Button</TomatoButton>
+
     </div>
+    
   );
 }
 
