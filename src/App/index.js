52d473485a9e9c152747308fae95fa5d85ad5db3
Jaikangam Malangmei <jaikangam12@gmail.com>
2020-04-10 00:02:44 +0530

    Add git AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -2,30 +2,13 @@ import React from 'react';
 import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components';
 import './App.css';
-
-const MyButton = styled.div`
-  color:green;
-
-  ${props => props.primary && css`
-    color: palevioletred;
-  `}
-`
-
-// A new component based on Button, but with some override styles
-const TomatoButton = styled(MyButton)`
-  color: tomato;
-  border-color: tomato;
-`;
+import AppLayout from './AppLayout';
 
 function App() {
   return (
-    <div> 
+    <AppLayout> 
       <WelcomeMessage />
-      <MyButton> Hello </MyButton>
-      <MyButton primary> Hello </MyButton>
-      <TomatoButton>Tomato Button</TomatoButton>
-
-    </div>
+    </AppLayout>
     
   );
 }
