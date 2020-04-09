cb242356be5b5f52e8d7f9fb97216891b05775b1
Jaikangam Malangmei <jaikangam12@gmail.com>
2020-04-10 00:36:02 +0530

    Added style app bar

--- src/App/index.js
+++ src/App/index.js
@@ -3,10 +3,12 @@ import WelcomeMessage from './WelcomeMessage'
 import styled, {css} from 'styled-components';
 import './App.css';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar'
 
 function App() {
   return (
     <AppLayout> 
+      <AppBar />
       <WelcomeMessage />
     </AppLayout>
     
