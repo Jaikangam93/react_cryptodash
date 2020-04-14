6c81d6839a18b743abf32f4be4e76c564877c79c
Jaikangam Malangmei <jaikangam12@gmail.com>
2020-04-14 07:51:38 +0530

    Added search input filed

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,13 +3,14 @@ import Welcomemessage from './WelcomeMessage';
 import ConfirmButton from './ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
-
+import Search from '../Settings/Search';
 
 export default function(){
     return <Page name="setting">
           <Welcomemessage /> 
           <CoinGrid topSection/>
           <ConfirmButton /> 
+          <Search/>
           <CoinGrid />
         </Page>
 }
